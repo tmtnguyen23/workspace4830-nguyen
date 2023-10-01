e647e16171654b69acb1df6d6716155c6a213d4c
thing <tmtnguyen@unomaha.edu>
2023-09-30 22:39:46 -0500

    Change hello world message displayed

--- webproject/src/main/java/MyServlet.java
+++ webproject/src/main/java/MyServlet.java
@@ -27,7 +27,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("hello world1");
+		response.getWriter().append("hello world");
 	}
 
 	/**
